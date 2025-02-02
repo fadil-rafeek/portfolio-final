dac30d8ae84366d5b77ba1bb53e7ec1524b3a8ed
fadil-rafeek <118721440+fadil-rafeek@users.noreply.github.com>
2025-02-02 05:02:27 +0400

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -24,9 +24,9 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
+        {" "}
+        <Link href="http://www.nil.com">
+          <a className="underline underline-offset-1">Fadil Rafeek</a>
         </Link>
       </h1>
     </>
