b63f438bea2979cecbdefdf2c5c2befa41ed0c73
fadil-rafeek <118721440+fadil-rafeek@users.noreply.github.com>
2025-02-05 01:30:20 +0400

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -9,7 +9,7 @@ const Footer = ({}) => {
       <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
         <div>
           <h1 className="text-2xl text-bold">Contact.</h1>
-          
+          <Socials />
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
